9e73881acee9188042b97ecbcf67abc25a269872
Atharvabodkhe <atharvarb12@gmail.com>
2024-01-19 15:33:15 +0530

    db connection

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,11 @@
+import { MongoClient } from 'mongodb';
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
+
+const uri = "mongodb+srv://atharvarb12:erEeFNpcaM2lxWOm@cluster0.rrdekpg.mongodb.net/";
+
+const client = new MongoClient(uri);
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
